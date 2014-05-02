26e4dd6f20661a60fad74b433c860be27581ee64
jalane <jalane@4658f029-88e0-4d37-8109-9890db1b95be>
2014-05-02 00:44:47 +0000

    Solved # 18

---
+++ p18.py
@@ -0,0 +1,38 @@
+#! python
+import sys
+import math
+import euler
+
+def get_left_parent(triangle, level, index):
+    parent_index = index - 1
+    parent_level = level - 1
+    if parent_index < 0:
+        return 0
+    else:
+        return triangle[parent_level][parent_index]
+
+def get_right_parent(triangle, level, index):
+    parent_index = index
+    parent_level = level - 1
+    if parent_index >= len(triangle[parent_level]):
+        return 0
+    else:
+        return triangle[parent_level][parent_index]
+
+def main():
+    with open('p18.dat') as f:
+        triangle = [[int(x) for x in line.split()] for line in f]
+
+    for level in range(1, len(triangle)):
+        for index in range(len(triangle[level])):
+            left = get_left_parent(triangle, level, index)
+            right = get_right_parent(triangle, level, index)
+            if left > right:
+                triangle[level][index] += left
+            else:
+                triangle[level][index] += right
+
+    print(max(triangle[-1]))
+
+if __name__ == "__main__":
+    main()
