e69782a03352b56f891bf42eec82e1a205685ec7
jalane <jalane@4658f029-88e0-4d37-8109-9890db1b95be>
2014-05-06 21:59:15 +0000

    Solved # 27

--- p27.py
+++ p27.py
@@ -4,8 +4,23 @@ import math
 import euler
 
 def main():
-    print('Hello!')
+    max_primes = 0
+    max_product = 0
+    for a in range(-999, 1000):
+        for b in range(-999, 1000):
+            n = 0
+            stop = False
+            while not stop:
+                value = n ** 2 + a * n + b
+                if not euler.is_prime(value):
+                    stop = True
+                n += 1
+            if n > max_primes:
+                max_primes = n
+                max_product = a * b
+    print('maximum product = ', max_product)
 
 if __name__ == "__main__":
     main()
+
 
