2802ac24640236bd3156e6367e802255b5bf6ad0
jalane <jalane@4658f029-88e0-4d37-8109-9890db1b95be>
2014-05-29 07:21:25 +0000

    Finished #61, but it's pretty slow.

--- p61.py
+++ p61.py
@@ -1,26 +1,41 @@
 #! python
 
 import euler
-import operator
+import itertools
 
 def is_cyclic(n, m):
     return n != m and str(n)[2:] == str(m)[:2]
 
 def main():
-    #check_funcs = [euler.is_triangle_number, euler.is_square_number, euler.is_pentagonal_number, euler.is_hexagonal_number, euler.is_heptagonal_number, euler.is_octagonal_number]
-    check_funcs = [euler.is_triangle_number, euler.is_square_number, euler.is_pentagonal_number]
+    check_funcs = [euler.is_triangle_number, euler.is_square_number, euler.is_pentagonal_number, euler.is_hexagonal_number, euler.is_heptagonal_number, euler.is_octagonal_number]
     polygonals = [[n for n in range(1000, 10000) if check_func(n)] for check_func in check_funcs]
-    S = [[p] for p in polygonals[0]]
-    for i in range(1, len(polygonals)):
-        new_S = []
-        polys = polygonals[i]
-        for s in S:
-            for p in polys:
-                if is_cyclic(s[-1], p):
-                    new_S = new_S + [s + [p]]
-        S = new_S
-    S = [s for s in S if is_cyclic(s[-1], s[0])]
-    print(S)
+    polygonals = list(itertools.chain(*polygonals))
+    print('There are ', len(polygonals), ' polygonal numbers.')
+
+    cycles = [[p] for p in polygonals]
+    for i in range(1, len(check_funcs)):
+        new_cycles = []
+        for c in cycles:
+            for p in polygonals:
+                if is_cyclic(c[-1], p):
+                    new_cycles = new_cycles + [c + [p]]
+        cycles = new_cycles
+        print('step ', i, ': ', len(cycles))
+    cycles = [sorted(c) for c in cycles if is_cyclic(c[-1], c[0])]
+    cycles = {tuple(c) for c in cycles}
+    cycles = [list(c) for c in cycles]
+
+    print('There are ', len(cycles), ' unique cycles.')
+
+    poly_cycles = []
+    for c in cycles[:]:
+        perms = itertools.permutations(c)
+        for p in perms:
+            if all([f(x) for f, x in zip(check_funcs, p)]):
+                poly_cycles.append(c)
+                break
+    print(poly_cycles)
+    print(sum(poly_cycles[0]))
 
 if __name__ == "__main__":
     main()
